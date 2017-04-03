8f08d66841e2ffdd4786980dd5fc4545edb1fbae
martins-giberts <martins.giberts@gmail.com>
2017-04-03 15:39:27 +0300

    Reusable socket connection by import

--- app/components/messages/index.jsx
+++ app/components/messages/index.jsx
@@ -1,15 +1,27 @@
-import React, {PropTypes} from 'react'
+import {observer} from 'mobx-react'
+import React, {Component, PropTypes} from 'react'
+import chatStore from '../../models/chatStore'
+import Message from '../../elements/message'
+import socket from '../../lib/clientSocket'
 import styles from './Messages.css'
 
-const Messages = ({
-  children,
-}) =>
-  <ul className={styles.messages}>
-    {children}
-  </ul>
+@observer
+class Messages extends Component {
+  componentDidMount() {
+    socket.on('chat message', function(message) {
+      chatStore.addMessage(message)
+    })
+  }
 
-Messages.propTypes = {
-  children: PropTypes.any,
+  render () {
+    return(
+      <ul className={styles.messages}>
+        {chatStore.messages.map((text, key) => {
+          return <Message key={key} text={text} />
+        })}
+      </ul>
+    )
+  }
 }
 
 export default Messages
